cd4845312cec6dc66e8d6ce054f3c9a772aa48a3
ADARSH-IIIT <adarshtech251@gmail.com>
2024-01-12 22:42:21 +0530

    updated origin localhost===> fronted hosted link

--- entry/server.js
+++ entry/server.js
@@ -21,7 +21,7 @@ connect_to_db(process.env.URL)
 
 server.use(cookiePARSER())
 server.use(express.json())
-server.use(   cors(  {credentials: true, origin: 'http://localhost:3000'}   )      )
+server.use(   cors(  {credentials: true, origin: 'https://chat-frontend-secure-o74pkd8l2-adarsh-dwivedis-projects.vercel.app'}   )      )
 
 
 
@@ -35,7 +35,7 @@ server.use(messageROUTER)
 
 const newserver = http.createServer(  server   )
 
-const BackendSwitch = new Server(  newserver  , {  cors : { origin : "http://localhost:3000" } } )
+const BackendSwitch = new Server(  newserver  , {  cors : { origin : "https://chat-frontend-secure-o74pkd8l2-adarsh-dwivedis-projects.vercel.app/" } } )
 
 
 
